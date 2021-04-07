e479063f527d1578adacd9c96efaa052f2863e34
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-04-07 19:42:53 +0700

    Add template service

---
+++ src/main/kotlin/com/vedblade/templateservice/TemplateService.kt
@@ -0,0 +1,24 @@
+package com.vedblade.templateservice
+
+import org.springframework.stereotype.Service
+
+@Service
+class TemplateService(
+	private val repository: TemplateRepository,
+	private val engine: TemplateEngine,
+	private val sendingService: SendingService,
+	private val mapper: TemplateMapper
+) {
+	fun save(templateDto: TemplateDto) {
+		val entity = mapper.dtoToEntity(templateDto)
+		repository.save(entity)
+	}
+
+	fun send(templateId: String, data: Map<String, String>) {
+		val template = repository.findById(templateId).orElseThrow()
+		val text = engine.process(template.template!!, data)
+		template.recipients!!.forEach { recipient ->
+			sendingService.send(endpoint = recipient, text = text)
+		}
+	}
+}
